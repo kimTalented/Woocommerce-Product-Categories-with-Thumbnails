668c535b3e19e857fc2a4d73cdfa9472e8ba7e26
Kimberley Griffith <kimberley.m.griffith@gmail.com>
2020-04-25 10:10:02 -0400

    Add files via upload

--- Woocommerce-Product-Categories-with-Thumbnails.php
+++ Woocommerce-Product-Categories-with-Thumbnails.php
@@ -4,43 +4,44 @@
 
 function showProductCat() {
 
-//Define category arguments
-$cat_args = array(
-'orderby' => 'name',
-'order' => 'asc',
-'hide_empty' => true,
-);
+	//Define category arguments
+	$cat_args = array(
+		'orderby' => 'name',
+		'order' => 'asc',
+		'hide_empty' => true,
+	);
 
-//Load categories using query
-$product_categories = get_terms( 'product_cat', $cat_args );
+	//Load categories using query
+	$product_categories = get_terms( 'product_cat', $cat_args );
 
-$result = "<div class='product-categories-with-image'>";
+	$result = "<div class='product-categories-with-image'>";
 
-if( !empty($product_categories) ){
+	if( !empty($product_categories) ){
 
-//Print each returned item
-foreach ($product_categories as $key => $category) {
+		//Print each returned item
+		foreach ($product_categories as $key => $category) {
 
-$thumb_id = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
-$term_img = D0d_get_attachment_image_src( $thumb_id, 'thumbnail', true );
+			$thumb_id = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
+			$term_img = wp_get_attachment_image_src( $thumb_id, 'thumbnail', true );
 
-$cat_img = "";
+			$cat_img = "";
 
-if ($term_img[3]) {
-$cat_img = "<img src='". $term_img[0] ."' width='" . $term_img[1] . "' height='" . $term_img[2] . "' />";
-}
+			if ($term_img[3]) {
+				$cat_img = "<img src='". $term_img[0] ."' width='" . $term_img[1] . "' height='" . $term_img[2] . "' />";
+			}
 
-$result = $result . '<a href="'.get_term_link($category).'" >'
-. $cat_img
-. $category->name
-. '</a><br/>';
-}
+			$result = $result . '<a href="'.get_term_link($category).'" >'
+			. $cat_img
+			. $category->name
+			. '</a><br/>';
+		}
 
-}
+	}
 
-$result = $result . "</div>";
+	$result = $result . "</div>";
 
-return $result;
+	return $result;
+	
 }
 add_shortcode('show-product-categories-with-image', 'showProductCat');
 
